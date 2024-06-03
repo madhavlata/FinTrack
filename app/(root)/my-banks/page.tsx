b5c5d549e0881f731ab2b12018e96b6f78bbe431
madhavlata <madhav10.500@gmail.com>
2024-06-03 18:13:05 +0530

    deploy errors resolve try

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -6,6 +6,9 @@ import React from "react";
 
 const MyBanks = async () => {
   const loggedIn = await getLoggedInUser();
+  if (!loggedIn && !loggedIn.$id) {
+    return null;
+  }
   const accounts = await getAccounts({
     userId: loggedIn.$id,
   });
